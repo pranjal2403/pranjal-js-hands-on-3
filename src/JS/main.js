2c27d0d5832d8b630b2ccffff86198f5f9de8308
pranjal2403 <pranjalsingh@cedcoss.com>
2023-03-15 10:09:51 +0530

    Comparisons with the Logical And Operator

---
+++ src/JS/main.js
@@ -0,0 +1,13 @@
+// Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+if (val<= 50 && val>=25) {
+  return "Yes";
+}
+return "No";
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
