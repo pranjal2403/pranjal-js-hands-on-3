23f70c5bf92f019dc25c98bbcc33b153cf6238ab
pranjal2403 <pranjalsingh@cedcoss.com>
2023-03-15 10:54:38 +0530

    Replacing If Else Chains with Switch

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,13 +1,28 @@
-// Comparisons with the Logical And Operator
-function testLogicalAnd(val) {
+// Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
   // Only change code below this line
 
-if (val<= 50 && val>=25) {
-  return "Yes";
-}
-return "No";
-  // Only change code above this line
-  return "No";
+ switch (val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+    break;
 }
 
-testLogicalAnd(10);
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
